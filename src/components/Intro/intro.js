39a76c6d5bad51fed88ea1f82a05beda22da4181
jagtapshubham2112 <156161489+jagtapshubham2112@users.noreply.github.com>
2024-08-17 21:42:21 +0530

    chages regarding the button of resume

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,22 +3,29 @@ import './intro.css';
 import bg from '../../assets/image.png';
 import rs from '../../assets/shubham_Resume/Resume.pdf';
 import resumeBtnImg from '../../assets/resume.png';
-// import { Link } from 'react-scroll';
 
 const Intro = () => {
+  const handleResumeDownload = (e) => {
+    e.preventDefault();
+    const link = document.createElement('a');
+    link.href = rs;
+    link.download = encodeURIComponent("Shubham's_Resume.pdf");
+    link.click();
+  };
+
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello, </span>
         <span className="introText">I'm <span className="introName">Shubham</span> <br /> Data Analyst </span>
         <p className='introPara'>I'm a budding data analyst with a passion for turning data into <br /> insights. Explore my portfolio to see my projects and learn about <br /> my analytical journey. </p>
-        <a href={rs} download={encodeURIComponent("Shubham's_Resume.pdf")}>
-          <button className="btn">
+        
+        <div className="buttonWrapper">
+          <button className="btn" onClick={handleResumeDownload}>
             <img src={resumeBtnImg} alt="Hire" className='btnImg' />
             Resume
           </button>
-        </a>
-
+        </div>
       </div>
       <img src={bg} alt="Profile" className="bg" />
     </section>
